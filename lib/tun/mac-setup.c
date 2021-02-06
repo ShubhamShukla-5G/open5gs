cad6efb18e67e2baae4614253a9c0868fa5fed64
Sukchan Lee <acetcom@gmail.com>
2021-02-06 10:56:57 -0500

    update it

--- lib/tun/mac-setup.c
+++ lib/tun/mac-setup.c
@@ -31,9 +31,11 @@
 #include <netinet6/in6_var.h>
 #include <netinet6/nd6.h>
 
+#if defined(__APPLE__)
 #include <net/if_utun.h>
 #include <sys/kern_event.h>
 #include <sys/kern_control.h>
+#endif
 
 static int unit = 0;
 
@@ -412,64 +414,4 @@ int ogs_tun_set_ip(char *ifname, ogs_ipsubnet_t *gw, ogs_ipsubnet_t *sub)
         rv = tun_set_ipv6(ifname, gw, sub);
 
     return rv;
-}
-
-ogs_pkbuf_t *ogs_tun_read(ogs_socket_t fd, ogs_pkbuf_pool_t *packet_pool)
-{
-    ogs_pkbuf_t *recvbuf = NULL;
-    int n;
-
-    ogs_assert(fd != INVALID_SOCKET);
-    ogs_assert(packet_pool);
-
-    recvbuf = ogs_pkbuf_alloc(packet_pool, OGS_MAX_PKT_LEN);
-    ogs_assert(recvbuf);
-    ogs_pkbuf_reserve(recvbuf, OGS_TUN_MAX_HEADROOM);
-    ogs_pkbuf_put(recvbuf, OGS_MAX_PKT_LEN-OGS_TUN_MAX_HEADROOM);
-
-    n = ogs_read(fd, recvbuf->data, recvbuf->len);
-    if (n <= 0) {
-        ogs_log_message(OGS_LOG_WARN, ogs_socket_errno, "ogs_read() failed");
-        ogs_pkbuf_free(recvbuf);
-        return NULL;
-    }
-
-    ogs_pkbuf_trim(recvbuf, n);
-
-    /* Remove Null/Loopback Header (4bytes) */
-    ogs_pkbuf_pull(recvbuf, 4);
-
-    return recvbuf;
-}
-
-int ogs_tun_write(ogs_socket_t fd, ogs_pkbuf_t *pkbuf)
-{
-    uint8_t version;
-    uint32_t family;
-
-    ogs_assert(fd != INVALID_SOCKET);
-    ogs_assert(pkbuf);
-
-    version = (*((unsigned char *)pkbuf->data) >> 4) & 0xf;
-
-    if (version == 4) {
-        family = htobe32(AF_INET);
-    } else if (version == 6) {
-        family = htobe32(AF_INET6);
-    } else {
-        ogs_error("Invalid packet [IP version:%d, Packet Length:%d]",
-                version, pkbuf->len);
-        ogs_log_hexdump(OGS_LOG_ERROR, pkbuf->data, pkbuf->len);
-        return OGS_ERROR;
-    }
-
-    ogs_pkbuf_push(pkbuf, sizeof(family));
-    memcpy(pkbuf->data, &family, sizeof(family));
-
-    if (ogs_write(fd, pkbuf->data, pkbuf->len) <= 0) {
-        ogs_log_message(OGS_LOG_ERROR, ogs_socket_errno, "ogs_write() failed");
-        return OGS_ERROR;
-    }
-
-    return OGS_OK;
 }
